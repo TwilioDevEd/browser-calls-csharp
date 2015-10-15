bbab68b71cbb4456e7cff7489bf0435345dc6224
Agustin Camino <agustin.camino@gmail.com>
2015-10-15 07:21:40 -0500

    Add reference to browser-calls.js

--- BrowserCalls.Web/App_Start/BundleConfig.cs
+++ BrowserCalls.Web/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace BrowserCalls.Web
 {
@@ -22,6 +21,9 @@ namespace BrowserCalls.Web
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/browser-calls").Include(
+                      "~/Scripts/browser-calls.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
